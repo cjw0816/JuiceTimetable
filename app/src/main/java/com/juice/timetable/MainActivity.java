799bcd76701edcfb3dff0e0d19a408e04223dd90
cjw0816 <394189874@qq.com>
2020-06-06 22:52:46 +0800

    fix:去除无用代码

--- app/src/main/java/com/juice/timetable/MainActivity.java
+++ app/src/main/java/com/juice/timetable/MainActivity.java
@@ -46,10 +46,6 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         //屏幕固定为竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // 设置状态栏为白底黑字
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }*/
         /**
          * 通知栏透明
          * SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
