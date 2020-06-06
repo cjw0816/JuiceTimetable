9f226c1b7c50aa8f848fd0978c1e93168c1f8eba
cjw0816 <394189874@qq.com>
2020-06-06 17:34:46 +0800

    fix:修改为继承Activity 解决问题

--- app/src/main/java/com/juice/timetable/StartPageActivity.java
+++ app/src/main/java/com/juice/timetable/StartPageActivity.java
@@ -1,29 +1,21 @@
 package com.juice.timetable;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.juice.timetable.utils.BaseActivity;
-
-public class StartPageActivity extends BaseActivity {
+public class StartPageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Thread myThread = new Thread() {//创建子线程T
-
-            @Override
-            public void run() {
-                try {
-                    sleep(1000);//使程序休眠一秒
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();//启动线程
-        Intent it = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(it);
-        finish();//关闭当前活动
+        try {
+            Thread.sleep(1500);//线程休眠1s，使出现白屏时的效果更加明显
+            Intent it = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(it);
+            finish();//关闭当前活动
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
 }
