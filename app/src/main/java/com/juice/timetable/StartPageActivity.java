b9ef24ca08728eb3065a9add001e82061f67fdce
cjw0816 <394189874@qq.com>
2020-06-06 22:41:53 +0800

    fix:启动页导航栏沉浸

--- app/src/main/java/com/juice/timetable/StartPageActivity.java
+++ app/src/main/java/com/juice/timetable/StartPageActivity.java
@@ -3,11 +3,13 @@ package com.juice.timetable;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class StartPageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         try {
             Thread.sleep(1500);//线程休眠1s，使出现白屏时的效果更加明显
             Intent it = new Intent(getApplicationContext(), MainActivity.class);
