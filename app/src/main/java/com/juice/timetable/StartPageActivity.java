fc4eacfabc341da35f038984099e1aa7ae9ecc06
cjw0816 <394189874@qq.com>
2020-06-06 22:51:57 +0800

    fix:还是改为继承Activity

--- app/src/main/java/com/juice/timetable/StartPageActivity.java
+++ app/src/main/java/com/juice/timetable/StartPageActivity.java
@@ -1,11 +1,10 @@
 package com.juice.timetable;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class StartPageActivity extends AppCompatActivity {
+public class StartPageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
