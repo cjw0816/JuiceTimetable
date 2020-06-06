e2b5ba35b7604f7b8796c11f2d064ea81a9783bd
cjw0816 <394189874@qq.com>
2020-06-06 22:48:34 +0800

    fix:将StartPageActivity改为继承AppCompatActivity 解决点击软件启动时会停顿的问题

--- app/src/main/java/com/juice/timetable/StartPageActivity.java
+++ app/src/main/java/com/juice/timetable/StartPageActivity.java
@@ -1,15 +1,14 @@
 package com.juice.timetable;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
 
-public class StartPageActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         try {
             Thread.sleep(1500);//线程休眠1s，使出现白屏时的效果更加明显
             Intent it = new Intent(getApplicationContext(), MainActivity.class);
